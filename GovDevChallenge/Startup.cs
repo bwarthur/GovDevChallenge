e505418d20c3ed365ca4d284e2389ab6aa9f81dd
kc0isg <kc0isg@gmail.com>
2014-05-17 12:39:08 -0600

    Initial Commit.

---
+++ GovDevChallenge/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GovDevChallenge.Startup))]
+namespace GovDevChallenge
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
