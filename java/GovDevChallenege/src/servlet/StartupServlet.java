1516db47453c6b683708c6feec9defcf73ede8fa
ryangpts <ryan@gannettpeaktech.com>
2014-05-17 17:30:16 -0600

    test

--- java/GovDevChallenege/src/servlet/StartupServlet.java
+++ java/GovDevChallenege/src/servlet/StartupServlet.java
@@ -2,24 +2,19 @@ package servlet;
 
 import javax.servlet.http.*;
 
+import com.googlecode.objectify.ObjectifyService;
+
+import entity.Test;
+
 public class StartupServlet extends HttpServlet {
 	static {
     	System.out.println("servlet.StartupServlet");
     	System.out.println("");
-    	
-    	System.out.println("ObjectifyService - registering translators:");
 
-//    	ObjectifyService.factory().getTranslators().add(new BigDecimalLongTranslatorFactory(10000));
-//
-//    	System.out.println("  BigDecimalLongTranslatorFactory(10000)");
-//    	
-//    	System.out.println("");
-//
-//    	
-//        System.out.println("ObjectifyService - registering entities:");
-//
-//        ObjectifyService.register(EgiUser.class);
-//    	System.out.println("  EgiUser.class");
+        System.out.println("ObjectifyService - registering entities:");
+
+       //ObjectifyService.register(Test.class);
+    	System.out.println("  Test.class");
    
     }
 }
