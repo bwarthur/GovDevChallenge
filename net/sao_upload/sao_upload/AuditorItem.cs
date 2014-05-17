5feda4f279745736fcd4eaaa6d7c017472eab3de
ryangpts <ryan@gannettpeaktech.com>
2014-05-17 15:34:42 -0600

    stuff

---
+++ net/sao_upload/sao_upload/AuditorItem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sao_upload
+{
+    public class AuditorItem
+    {
+        public string PaymentDate { get; set; }
+        public string Type { get; set; }
+        public decimal WarrantEFTAmt { get; set; }
+        public string WarrantEFTAmtSign { get; set; }
+        public string LineAmt { get; set; }
+        public string LineAmtSign { get; set; }
+        public string Vendor { get; set; }
+        public string VendorName { get; set; }
+        public string Dept { get; set; }
+        public string Document { get; set; }
+        public string Invoice { get; set; }
+        public string PaymentDescription { get; set; }
+        public string DeptName { get; set; }
+        public string DeptContact { get; set; }
+        public string WarrantEFT { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder s = new StringBuilder();
+
+            s.Append(PaymentDate + Environment.NewLine);
+            s.Append(Type + Environment.NewLine);
+            s.Append(WarrantEFTAmt + Environment.NewLine);
+            s.Append(WarrantEFTAmtSign + Environment.NewLine);
+            s.Append(LineAmt + Environment.NewLine);
+            s.Append(LineAmtSign + Environment.NewLine);
+            s.Append(Vendor + Environment.NewLine);
+            s.Append(VendorName + Environment.NewLine);
+            s.Append(Dept + Environment.NewLine);
+            s.Append(Document + Environment.NewLine);
+            s.Append(Invoice + Environment.NewLine);
+            s.Append(PaymentDescription + Environment.NewLine);
+            s.Append(DeptName + Environment.NewLine);
+            s.Append(DeptContact + Environment.NewLine);
+            s.Append(WarrantEFT + Environment.NewLine);
+
+            return s.ToString();
+        }
+    }
+}
---
+++ net/sao_upload/sao_upload/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+
+namespace sao_upload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+            string path = @"C:\projects\GovDevChallenge\net\data\Vendor Payments - ETS Data Request.csv";
+
+            var list = new List<AuditorItem>();
+
+            using (TextReader textReader = new StreamReader(path))
+            {
+                using (var csvReader = new CsvReader(textReader))
+                {
+                    while (csvReader.Read())
+                    {
+                        var auditorItem = new AuditorItem();
+
+                        auditorItem.PaymentDate = csvReader.GetField<string>(0);
+                        auditorItem.Type = csvReader.GetField<string>(1);
+                        auditorItem.WarrantEFTAmt = csvReader.GetField<decimal>(2);
+                        auditorItem.WarrantEFTAmtSign = csvReader.GetField<string>(3);
+                        auditorItem.LineAmt = csvReader.GetField<string>(4);
+                        auditorItem.LineAmtSign = csvReader.GetField<string>(5);
+                        auditorItem.Vendor = csvReader.GetField<string>(6);
+                        auditorItem.VendorName = csvReader.GetField<string>(7);
+                        auditorItem.Dept = csvReader.GetField<string>(8);
+                        auditorItem.Document = csvReader.GetField<string>(9);
+                        auditorItem.Invoice = csvReader.GetField<string>(10);
+                        auditorItem.PaymentDescription = csvReader.GetField<string>(11);
+                        auditorItem.DeptName = csvReader.GetField<string>(12);
+                        auditorItem.DeptContact = csvReader.GetField<string>(13);
+                        auditorItem.WarrantEFT = csvReader.GetField<string>(14);
+
+                        list.Add(auditorItem);
+                    }
+                }
+            }
+
+            //new CsvReader()
+
+            //using (var streamReader = new StreamReader(path))
+            //{
+            //    string line;
+            //    var lineCount = 0;
+            //    while((line = streamReader.ReadLine()) != null)
+            //    {
+            //        if (lineCount > 0 && lineCount < 10)
+            //        {
+            //            var lineSplit = line.Split(',');
+            //            var auditorItem = new AuditorItem()
+            //            {
+            //                PaymentDate = Convert.ToDateTime(lineSplit[0]),
+            //                Type = lineSplit[1],
+            //                WarrantEFTAmt = Convert.ToDecimal(lineSplit[2]),
+            //                WarrantEFTAmtSign = lineSplit[3],
+            //                LineAmt = lineSplit[4],
+            //                LineAmtSign = lineSplit[5],
+            //                Vendor = lineSplit[6],
+            //                VendorName = lineSplit[7],
+            //                Dept = lineSplit[8],
+            //                Document = lineSplit[9],
+            //                Invoice = lineSplit[10],
+            //                PaymentDescription = lineSplit[11],
+            //                DeptName = lineSplit[12],
+            //                DeptContact = lineSplit[13],
+            //                WarrantEFT = lineSplit[14]
+            //            };
+
+            //            list.Add(auditorItem);
+            //        }
+                    
+            //        lineCount += 1;
+            //    }
+            //}
+
+            //foreach (var line in list)
+            //{
+            //    Trace.WriteLine(line);
+            //}
+            //int i = 0;
+        }
+    }
+}
