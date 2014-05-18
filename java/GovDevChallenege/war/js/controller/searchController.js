09eefaa9fdb65f87140d54a491be317e72e1fe19
ryangpts <ryan@gannettpeaktech.com>
2014-05-17 19:22:12 -0600

    test

--- java/GovDevChallenege/war/js/controller/searchController.js
+++ java/GovDevChallenege/war/js/controller/searchController.js
@@ -1,4 +1,31 @@
 app.controller("searchController", function($scope) 
 {
-	$scope.description = "searchController";
+	var isDebug = true;
+	$scope.serviceName = "searchController";
+	this.endPointPath = "/auditorEndpoint/v1";
+	
+	console.log($scope.serviceName);
+	
+    $scope.searchX = function()
+    {
+    	console.log('searchX');
+    	alert('test');
+    	
+    	serviceClientService.put(isDebug, $scope.serviceName, this.endPointPath + "/search", searchCriteria).then(function(data) {
+    		console.log(data);
+    	});
+    	
+//        loggingService.debug(isDebug, $scope.employeeSearchCriteria);
+//
+//        employeeService.search($scope.employeeSearchCriteria).then(function(data){
+//            if (data.recordCount == 0)
+//                $scope.allData = [];
+//            else
+//                $scope.allData = data.items;
+//
+//        }, function(error){
+//            loggingService.error(error);
+//        });
+    }
+	
 });
---
+++ java/GovDevChallenege/war/js/service/serviceClientService.js
@@ -0,0 +1,50 @@
+app.service('serviceClientService', function($q, $http){
+	"use strict";
+
+	var wrappingServiceName = "serviceClientService";
+	var urlBase = "/_ah/api";
+    
+	this.get = function(isDebug, serviceName, url) {
+        var methodName = wrappingServiceName + " - " + serviceName + " - get";
+
+        var fullUrl = urlBase + url;
+        var debugString = methodName + ' - ajax - ' + fullUrl;
+
+        var defer = $q.defer();
+
+        $http.get(fullUrl).success(function(data, status, headers, config)
+        {
+            if (data != null && data != '') {
+                defer.resolve(data);
+            } else {
+                defer.resolve(null);
+            }
+        }).error(function(data, status, headers, config){
+            defer.reject(status);
+        });
+
+        return defer.promise;
+    }
+
+	this.put = function(isDebug, serviceName, url, data) {
+		var methodName = wrappingServiceName + " - " + serviceName + " - put";
+
+        var fullUrl = urlBase + url;
+        var debugString = methodName + ' - ajax - ' + fullUrl;
+
+        var defer = $q.defer();        
+        
+    	$http.post(fullUrl, data).success(function(data, status, headers, config)
+    	{
+	        if (data != null && data != '') {
+	            defer.resolve(data);
+	        } else {
+	            defer.resolve(null);
+	        }
+    	}).error(function(data, status, headers, config){
+    		defer.reject(status);
+    	});        	
+
+        return defer.promise;
+	}			
+});
